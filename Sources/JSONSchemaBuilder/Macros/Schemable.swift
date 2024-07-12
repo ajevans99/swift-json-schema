369f29d5c64fd3cf945d249e0f2794f73ef37b10
ajevans99 <ajevans99@users.noreply.github.com>
2024-07-12 21:56:49 +0000

    Apply auto-formatting

--- Sources/JSONSchemaBuilder/Macros/Schemable.swift
+++ Sources/JSONSchemaBuilder/Macros/Schemable.swift
@@ -1,5 +1,4 @@
 @attached(extension, conformances: Schemable) @attached(member, names: named(schema))
-public macro Schemable() =
-  #externalMacro(module: "JSONSchemaMacro", type: "SchemableMacro")
+public macro Schemable() = #externalMacro(module: "JSONSchemaMacro", type: "SchemableMacro")
 
 public protocol Schemable { static var schema: JSONSchemaRepresentable { get } }
