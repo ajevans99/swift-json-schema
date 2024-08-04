a19b32fc967bee8acce88b427b05d8e73d39f5e3
Austin Evans <austinjevans@me.com>
2024-08-04 14:35:21 -0400

    Fix for Xcode 16 Beta 4

--- Sources/JSONSchemaClient/main.swift
+++ Sources/JSONSchemaClient/main.swift
@@ -21,97 +21,104 @@ func printSchema<T: Schemable>(_ schema: T.Type) {
   }
 }
 
-//@Schemable struct Weather {
-//  @SchemaOptions(
-//    title: "Temperature",
-//    description: "The current temperature in fahrenheit, like 70°F",
-//    default: 75.0,
-//    examples: [72.0, 75.0, 78.0],
-//    readOnly: true,
-//    writeOnly: false,
-//    deprecated: true,
-//    comment: "This is a comment about temperature"
-//  ) @NumberOptions(multipleOf: 5, minimum: 0, maximum: 100) let temperature: Double
-//
-//  @SchemaOptions(
-//    title: "Humidity",
-//    description: "The current humidity percentage",
-//    default: 50,
-//    examples: [40, 50, 60],
-//    readOnly: false,
-//    writeOnly: true,
-//    deprecated: false,
-//    comment: "This is a comment about humidity"
-//  ) let humidity: Int
-//
+@Schemable struct Weather {
+  @SchemaOptions(
+    title: "Temperature",
+    description: "The current temperature in fahrenheit, like 70°F",
+    default: 75.0,
+    examples: [72.0, 75.0, 78.0],
+    readOnly: true,
+    writeOnly: false,
+    deprecated: true,
+    comment: "This is a comment about temperature"
+  ) @NumberOptions(multipleOf: 5, minimum: 0, maximum: 100) let temperature: Double
+
+  @SchemaOptions(
+    title: "Humidity",
+    description: "The current humidity percentage",
+    default: 50,
+    examples: [40, 50, 60],
+    readOnly: false,
+    writeOnly: true,
+    deprecated: false,
+    comment: "This is a comment about humidity"
+  ) let humidity: Int
+
 //  @SchemaOptions(title: "Temperature Readings")
 //  @ArrayOptions(minContains: 1, maxContains: 5, minItems: 2, maxItems: 10, uniqueItems: true)
 //  let temperatureReadings: [Double]
-//
-//  @SchemaOptions(title: "Location")
-//  @StringOptions(minLength: 5, maxLength: 100, pattern: "^[a-zA-Z]+$", format: nil) let cityName:
-//    String
-//}
 
-//let now = Weather(
-//  temperature: 72,
-//  humidity: 30,
+  @SchemaOptions(title: "Location")
+  @StringOptions(minLength: 5, maxLength: 100, pattern: "^[a-zA-Z]+$", format: nil) let cityName:
+    String
+}
+
+let now = Weather(
+  temperature: 72,
+  humidity: 30,
 //  temperatureReadings: [32, 70.1, 84],
-//  cityName: "Detroit"
-//)
-//
-//extension Weather: Codable {}
-//
-//printInstance(now)
-//printSchema(Weather.self)
+  cityName: "Detroit"
+)
 
-//@Schemable struct Book {
-//  let title: String
+extension Weather: Codable {}
+
+printInstance(now)
+printSchema(Weather.self)
+
+@Schemable struct Book {
+  let title: String
 //  let authors: [String]
-//  let yearPublished: Int
-//  @ExcludeFromSchema let rating: Double
-//}
-//
-//@Schemable struct Library {
-//  let name: String
-//  var books: [Book] = []
-//}
+  let yearPublished: Int
+  @ExcludeFromSchema let rating: Double
 
-//printSchema(Book.self)
-//printSchema(Library.self)
+  init(title: String, yearPublished: Int) {
+    self.title = title
+    self.yearPublished = yearPublished
+    self.rating = 1
+  }
+
+}
+
+@Schemable struct Library {
+  let name: String
+//  var books: [Book] = []
+}
+
+printSchema(Book.self)
+printSchema(Library.self)
 
 // MARK: - Enums
 
-//@Schemable enum TemperatureType: Codable {
-//  case fahrenheit
-//  case celcius
-//}
-//
-//printSchema(TemperatureType.self)
-//
-//@Schemable enum WeatherCondition: Codable { case sunny(hoursOfSunlight: Int)
-//  case hail(Bool)
-//  case cloudy(coverage: Double)
-//  case rainy(chanceOfRain: Double, amount: Double)
-//  case snowy
-//  case windy
-//  case stormy
-//}
-//
-//let conditions = WeatherCondition.sunny(hoursOfSunlight: 5)
-//printInstance(conditions)
-//printSchema(WeatherCondition.self)
-//
-//@Schemable enum Category { case fiction, nonFiction, science, history, kids, entertainment }
+@Schemable enum TemperatureType: Codable {
+  case fahrenheit
+  case celcius
+}
+
+printSchema(TemperatureType.self)
+
+@Schemable enum WeatherCondition: Codable { case sunny(hoursOfSunlight: Int)
+  case hail(Bool)
+  case cloudy(coverage: Double)
+  case rainy(chanceOfRain: Double, amount: Double)
+  case snowy
+  case windy
+  case stormy
+}
+
+let conditions = WeatherCondition.sunny(hoursOfSunlight: 5)
+printInstance(conditions)
+printSchema(WeatherCondition.self)
+
+@Schemable enum Category { case fiction, nonFiction, science, history, kids, entertainment }
 
 // MARK: Parsing
 
-enum Weather {
+enum WeatherType {
   case sunny
   case cloudy
   case rainy
 
-  static var schema: some JSONSchemaComponent<Weather> {
+  static var schema: some JSONSchemaComponent<WeatherType> {
     JSONEnum {
       "sunny"
       "cloudy"
@@ -138,9 +145,9 @@ enum Weather {
   }
 }
 
-let sunny = Weather.schema.validate(.string("sunny"))
+let sunny = WeatherType.schema.validate(.string("sunny"))
 print(sunny)
-let notSupported = Weather.schema.validate(.string("other"))
+let notSupported = WeatherType.schema.validate(.string("other"))
 print(notSupported)
 
 enum Airline: String, CaseIterable, Schemable {
