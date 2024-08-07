07b8742f920cc1f5007483f54a35c4a32e464f35
Austin Evans <austinjevans@me.com>
2024-08-07 01:17:26 -0400

    Fix for Xcode Beta 4

--- Tests/JSONSchemaMacroTests/Helpers/MacroExpansion+SwiftTesting.swift
+++ Tests/JSONSchemaMacroTests/Helpers/MacroExpansion+SwiftTesting.swift
@@ -18,25 +18,15 @@ func assertMacroExpansion(
     macroSpecs: macros.mapValues { MacroSpec(type: $0) },
     indentationWidth: .spaces(2),
     failureHandler: { spec in
-      // Xcode 16 Beta 1 and 2
       Issue.record(
         "\(spec.message)",
-        fileID: spec.location.fileID,
-        filePath: spec.location.filePath,
-        line: spec.location.line,
-        column: spec.location.column
+        sourceLocation: SourceLocation(
+          fileID: spec.location.fileID,
+          filePath: spec.location.filePath,
+          line: spec.location.line,
+          column: spec.location.column
+        )
       )
-
-      // Xcode 16 Beta 3
-      // Issue.record(
-      //   "\(spec.message)",
-      //   sourceLocation: SourceLocation(
-      //     fileID: spec.location.fileID,
-      //     filePath: spec.location.filePath,
-      //     line: spec.location.line,
-      //     column: spec.location.column
-      //   )
-      // )
     },
     fileID: fileID,
     filePath: filePath,
