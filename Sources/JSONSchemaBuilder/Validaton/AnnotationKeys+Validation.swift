f4ed59293e38be8fd12ca35a9dd8a443c6089f55
Austin Evans <austinjevans@me.com>
2024-08-28 23:23:29 -0400

    Add keys

--- Sources/JSONSchemaBuilder/Validaton/AnnotationKeys+Validation.swift
+++ Sources/JSONSchemaBuilder/Validaton/AnnotationKeys+Validation.swift
@@ -3,19 +3,40 @@ import JSONSchema
 extension AnnotationResults {
   // MARK: Array related
 
-  static let prefixItems = PrefixItemsKey.self
-  static let items = ItemsKey.self
-  static let contains = ContainsKey.self
+  public var prefixItems: PrefixItemsKey.ValueType? {
+    get { self[PrefixItemsKey.self] }
+    set { self[PrefixItemsKey.self] = newValue }
+  }
+
+  public var items: ItemsKey.ValueType? {
+    get { self[ItemsKey.self] }
+    set { self[ItemsKey.self] = newValue }
+  }
+
+  public var contains: ContainsKey.ValueType? {
+    get { self[ContainsKey.self] }
+    set { self[ContainsKey.self] = newValue }
+  }
 
   // MARK: Object related
 
-  static let properties = PropertiesKey.self
-  static let patternProperties = PatternPropertiesKey.self
-  static let additionalProperties = AdditionalPropertiesKey.self
+  public var properties: PropertiesKey.ValueType? {
+    get { self[PropertiesKey.self] }
+    set { self[PropertiesKey.self] = newValue }
+  }
+
+  public var patternProperties: PatternPropertiesKey.ValueType? {
+    get { self[PatternPropertiesKey.self] }
+    set { self[PatternPropertiesKey.self] = newValue }
+  }
+
+  public var additionalProperties: AdditionalPropertiesKey.ValueType? {
+    get { self[AdditionalPropertiesKey.self] }
+    set { self[AdditionalPropertiesKey.self] = newValue }
+  }
 }
 
 public enum PrefixItemsKey: AnnotationKey, Sendable {
-  public static let key = "prefixItems"
   public typealias ValueType = Self
 
   /// The largest index to which this keyword applied a subschema
@@ -25,14 +46,11 @@ public enum PrefixItemsKey: AnnotationKey, Sendable {
 }
 
 public enum ItemsKey: AnnotationKey {
-  public static let key = "items"
-
   /// True when keywords validates on all (after prefix items)
   public typealias ValueType = Bool
 }
 
 public enum ContainsKey: AnnotationKey, Sendable {
-  public static let key = "contains"
   public typealias ValueType = Self
 
   /// Array of indicies to which the keyword validates
@@ -42,20 +60,16 @@ public enum ContainsKey: AnnotationKey, Sendable {
 }
 
 public enum PropertiesKey: AnnotationKey {
-  public static let key = "properties"
   /// The set of instance property names matched by this keyword
   public typealias ValueType = Set<String>
 }
 
 public enum PatternPropertiesKey: AnnotationKey {
-  public static let key = "patternProperties"
   /// The set of instance property names matched by this keyword
   public typealias ValueType = Set<String>
 }
 
 public enum AdditionalPropertiesKey: AnnotationKey {
-  public static let key = "additionalProperties"
   /// Set of instance property names validated by the keyword's subschema
   public typealias ValueType = Set<String>
 }
-
