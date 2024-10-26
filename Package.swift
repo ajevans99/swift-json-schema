78c93f3f4de55bb6690412289e9036dacd59d97d
Austin Evans <austinjevans@me.com>
2024-10-26 14:07:41 -0400

    CI step for Apple platforms (#47)
    ## Description
    
    Fixes `Package.swift` platforms and add CI steps to build on Apple
    platforms
    
    Closes #30 
    
    ## Type of Change
    
    - [x] Bug fix
    - [ ] New feature
    - [ ] Breaking change
    - [ ] Documentation update
    
    ## Additional Notes
    
    Add any other context or screenshots about the pull request here.
    
    ---
    
    **Note:** You can add the `auto-format` label to this pull request to
    enable automatic Swift formatting.

--- Package.swift
+++ Package.swift
@@ -7,9 +7,9 @@ let package = Package(
   name: "swift-json-schema",
   platforms: [
     .macOS(.v14),
-    .iOS(.v13),
-    .watchOS(.v6),
-    .tvOS(.v13),
+    .iOS(.v17),
+    .watchOS(.v10),
+    .tvOS(.v17),
     .macCatalyst(.v14),
     .visionOS(.v1),
   ],
