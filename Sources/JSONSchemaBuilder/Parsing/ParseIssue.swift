33491db852404343f21c4a046740b3dd7c0446d6
Austin Evans <austinjevans@me.com>
2025-01-06 12:39:21 -0500

    Constrain Parsed Error to Swift Error (#57)
    ## Description
    
    This PR updates the Parsed<Value, Error> enum to constrain the Error
    generic to conform to Swift.Error. This change ensures semantic clarity,
    improves type safety, and enables better integration with Swift’s
    error-handling mechanisms.
    
    Closes #56 
    
    ## Type of Change
    
    - [ ] Bug fix
    - [x] New feature
    - [ ] Breaking change
    - [ ] Documentation update
    
    ## Additional Notes
    
    Add any other context or screenshots about the pull request here.
    
    ---
    
    **Note:** You can add the `auto-format` label to this pull request to
    enable automatic Swift formatting.
    
    ---------
    
    Co-authored-by: Guillaume Sabran <sabranguillaume@gmail.com>

--- Sources/JSONSchemaBuilder/Parsing/ParseIssue.swift
+++ Sources/JSONSchemaBuilder/Parsing/ParseIssue.swift
@@ -1,9 +1,26 @@
 import JSONSchema
 
-public enum ParseIssue: Equatable, Sendable {
+public enum ParseIssue: Error, Equatable, Sendable {
   case typeMismatch(expected: JSONType, actual: JSONValue)
   case noEnumCaseMatch(value: JSONValue)
   case missingRequiredProperty(property: String)
   case compactMapValueNil(value: JSONValue)
   case compositionFailure(type: JSONComposition, reason: String, nestedErrors: [ParseIssue])
+}
+
+extension ParseIssue: CustomStringConvertible {
+  public var description: String {
+    switch self {
+    case .typeMismatch(let expected, let actual):
+      "Type mismatch: the instance of type `\(actual.primative)` does not match the expected type `\(expected)`."
+    case .noEnumCaseMatch(let value):
+      "The instance `\(value)` does not match any enum case."
+    case .missingRequiredProperty(let property):
+      "Missing required property `\(property)`."
+    case .compactMapValueNil(let value):
+      "The instance `\(value)` returned nil when evaluated against compact map."
+    case .compositionFailure(let type, let reason, _):
+      "Composition (`\(type)`) failure: the instance \(reason)."
+    }
+  }
 }
