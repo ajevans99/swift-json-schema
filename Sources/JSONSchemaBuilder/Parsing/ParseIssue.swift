508dd0bec668c4568c8b83ebe10d5087d6c6eead
Austin Evans <austinjevans@me.com>
2025-01-05 23:03:15 -0500

    Add error descriptions for ParseIssue

--- Sources/JSONSchemaBuilder/Parsing/ParseIssue.swift
+++ Sources/JSONSchemaBuilder/Parsing/ParseIssue.swift
@@ -7,3 +7,20 @@ public enum ParseIssue: Error, Equatable, Sendable {
   case compactMapValueNil(value: JSONValue)
   case compositionFailure(type: JSONComposition, reason: String, nestedErrors: [ParseIssue])
 }
+
+extension ParseIssue: CustomStringConvertible {
+  public var description: String {
+    switch self {
+    case .typeMismatch(let expected, let actual):
+      "Type mismatch: the instance `\(actual)` is not of type `\(expected)`."
+    case .noEnumCaseMatch(let value):
+      "The instance `\(value)` does not match any enum case."
+    case .missingRequiredProperty(let property):
+      "Missing required property `\(property)`."
+    case .compactMapValueNil(let value):
+      "The instance `\(value)` returned nil when evulated against compact map."
+    case .compositionFailure(let type, let reason, let nestedErrors):
+      "Componsition (`\(type)`) failure: the instance \(reason). \(nestedErrors.map(\.description).joined(separator: "\n"))"
+    }
+  }
+}
