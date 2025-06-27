d7ae8430080bb3a6f0fa302fa08be294c292351a
Austin Evans <austinjevans@me.com>
2025-06-27 13:17:34 -0400

    Constant static conversions

--- Sources/JSONSchemaConversion/Conversions.swift
+++ Sources/JSONSchemaConversion/Conversions.swift
@@ -2,11 +2,11 @@ import Foundation
 import JSONSchemaBuilder
 
 public enum Conversions {
-  public static var uuid = UUIDConversion.self
+  public static let uuid = UUIDConversion.self
 
-  public static var dateTime = DateTimeConversion.self
-  public static var date = DateConversion.self
-  public static var time = TimeConversion.self
+  public static let dateTime = DateTimeConversion.self
+  public static let date = DateConversion.self
+  public static let time = TimeConversion.self
 
-  public static var url = URLConversion.self
+  public static let url = URLConversion.self
 }
