7b5300e7e3b3952764811a44d6109500ef5bf9de
Andre Navarro <andre.navarro@gmail.com>
2025-11-04 02:21:28 +0100

    lint

--- Sources/JSONSchemaBuilder/Macros/Schemable.swift
+++ Sources/JSONSchemaBuilder/Macros/Schemable.swift
@@ -6,20 +6,20 @@ public macro Schemable(
 
 public protocol Schemable {
   #if compiler(>=5.9)
-  @available(macOS 14.0, iOS 17.0, watchOS 10.0, tvOS 17.0, *)
-  associatedtype Schema: JSONSchemaComponent
+    @available(macOS 14.0, iOS 17.0, watchOS 10.0, tvOS 17.0, *)
+    associatedtype Schema: JSONSchemaComponent
 
-  @available(macOS 14.0, iOS 17.0, watchOS 10.0, tvOS 17.0, *)
-  @JSONSchemaBuilder static var schema: Schema { get }
+    @available(macOS 14.0, iOS 17.0, watchOS 10.0, tvOS 17.0, *)
+    @JSONSchemaBuilder static var schema: Schema { get }
 
-  @available(macOS 14.0, iOS 17.0, watchOS 10.0, tvOS 17.0, *)
-  static var keyEncodingStrategy: KeyEncodingStrategies { get }
+    @available(macOS 14.0, iOS 17.0, watchOS 10.0, tvOS 17.0, *)
+    static var keyEncodingStrategy: KeyEncodingStrategies { get }
   #endif
 }
 
 #if compiler(>=5.9)
-@available(macOS 14.0, iOS 17.0, watchOS 10.0, tvOS 17.0, *)
-extension Schemable {
-  public static var keyEncodingStrategy: KeyEncodingStrategies { .identity }
-}
+  @available(macOS 14.0, iOS 17.0, watchOS 10.0, tvOS 17.0, *)
+  extension Schemable {
+    public static var keyEncodingStrategy: KeyEncodingStrategies { .identity }
+  }
 #endif
