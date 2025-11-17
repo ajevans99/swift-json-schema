c458c3ace6eb06baf45c4492e0e70281c0f5e22e
Austin Evans <austinjevans@me.com>
2025-11-17 01:35:38 -0500

    lint

--- Sources/JSONSchema/Validation/SchemaDocument.swift
+++ Sources/JSONSchema/Validation/SchemaDocument.swift
@@ -4,11 +4,6 @@ struct SchemaDocument: Sendable {
   let url: URL
   let rawSchema: JSONValue
 
-  init(url: URL, rawSchema: JSONValue) {
-    self.url = url
-    self.rawSchema = rawSchema
-  }
-
   func value(at pointer: JSONPointer) -> JSONValue? {
     rawSchema.value(at: pointer)
   }
